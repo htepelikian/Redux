5d8602b7a9ebdba355448fef53887385f93dc08f
Hripsime Tepelikian <htepelikian@gmail.com>
2021-08-31 20:00:10 -0700

    product item update

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -6,17 +6,14 @@ import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
 
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  const state = useSelector((state) => {
+    return state
+  });
+  const dispatch = useDispatch();
 
-  const { cart } = state
+  const { cart } = state;
+
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
@@ -38,6 +35,14 @@ function ProductItem(item) {
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   }
+
+  const {
+    image,
+    name,
+    _id,
+    price,
+    quantity
+  } = item;
 
   return (
     <div className="card px-1 py-1">
