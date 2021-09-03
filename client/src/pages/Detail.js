3aea054955564eb155e639e255bdba5691afe322
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-03 13:41:07 -0700

    redux packages

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_PRODUCTS } from "../utils/queries";
+import spinner from '../assets/spinner.gif'
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
 } from '../utils/actions';
-import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
-import spinner from '../assets/spinner.gif';
 
 function Detail() {
 
